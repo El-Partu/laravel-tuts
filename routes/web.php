031baf9ce26f7cc3831bd9097755aca6a4230320
El-Partu <laurentpartu@gmail.com>
2024-10-07 19:59:05 +0000

    refactored route using resource route

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,15 @@ use App\Models\Job;
 
 Route::view('/','home');
 
-Route::controller(JobController::class)->group(function(){
-    Route::get('/jobs',  'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit',  'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'delete');
-});
+// Route::controller(JobController::class)->group(function(){
+//     Route::get('/jobs',  'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit',  'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'delete');
+// });
+Route::resource('jobs', JobController::class);
 Route::view('/contact', 'contact');
 //All Jobs
