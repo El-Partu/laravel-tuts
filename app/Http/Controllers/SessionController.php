63180baf78a2ac06e902e28c47038cbb5665bd1e
El-Partu <laurentpartu@gmail.com>
2024-10-08 11:37:43 +0000

    implemented logout functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
@@ -11,5 +12,8 @@ class SessionController extends Controller
     }
     public function store(){
         dd('Hey! I love Jesus Christ with all my soul, body and spirit');
+    }
+    public function destroy(){
+        Auth::logout();
     }
 }
